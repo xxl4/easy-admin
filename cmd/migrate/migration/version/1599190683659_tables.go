d68aac38f0c7e67369213da8bf0a4a44ef1893a4
Steve <nice.lizhi@gmail.com>
2023-11-02 19:16:26 +0800

    add cmd

---
+++ cmd/migrate/migration/version/1599190683659_tables.go
@@ -0,0 +1,54 @@
+package version
+
+import (
+	"runtime"
+
+	"github.com/nicelizhi/easy-admin-core/sdk/config"
+
+	"github.com/nicelizhi/easy-admin/cmd/migrate/migration"
+	"github.com/nicelizhi/easy-admin/cmd/migrate/migration/models"
+	common "github.com/nicelizhi/easy-admin/common/models"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+	_, fileName, _, _ := runtime.Caller(0)
+	migration.Migrate.SetVersion(migration.GetFilename(fileName), _1599190683659Tables)
+}
+
+func _1599190683659Tables(db *gorm.DB, version string) error {
+	return db.Transaction(func(tx *gorm.DB) error {
+		if config.DatabaseConfig.Driver == "mysql" {
+			tx = tx.Set("gorm:table_options", "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4")
+		}
+		err := tx.Migrator().AutoMigrate(
+			new(models.SysDept),
+			new(models.SysConfig),
+			new(models.SysTables),
+			new(models.SysColumns),
+			new(models.SysMenu),
+			new(models.SysLoginLog),
+			new(models.SysOperaLog),
+			new(models.SysRoleDept),
+			new(models.SysUser),
+			new(models.SysRole),
+			new(models.SysPost),
+			new(models.DictData),
+			new(models.DictType),
+			new(models.SysJob),
+			new(models.SysConfig),
+			new(models.SysApi),
+			new(models.TbDemo),
+		)
+		if err != nil {
+			return err
+		}
+		if err := models.InitDb(tx); err != nil {
+			return err
+		}
+		return tx.Create(&common.Migration{
+			Version: version,
+		}).Error
+	})
+}
---
+++ cmd/migrate/server.go
@@ -0,0 +1,107 @@
+package migrate
+
+import (
+	"bytes"
+	"fmt"
+	"strconv"
+	"text/template"
+	"time"
+
+	"github.com/nicelizhi/easy-admin-core/config/source/file"
+	"github.com/nicelizhi/easy-admin-core/sdk"
+	"github.com/nicelizhi/easy-admin-core/sdk/config"
+	"github.com/nicelizhi/easy-admin-core/sdk/pkg"
+
+	"github.com/spf13/cobra"
+
+	"github.com/nicelizhi/easy-admin/cmd/migrate/migration"
+	_ "github.com/nicelizhi/easy-admin/cmd/migrate/migration/version"
+	_ "github.com/nicelizhi/easy-admin/cmd/migrate/migration/version-local"
+	"github.com/nicelizhi/easy-admin/common/database"
+	"github.com/nicelizhi/easy-admin/common/models"
+)
+
+var (
+	configYml string
+	generate  bool
+	goAdmin   bool
+	host      string
+	StartCmd  = &cobra.Command{
+		Use:     "migrate",
+		Short:   "Initialize the database",
+		Example: "easy-admin migrate -c config/settings.yml",
+		Run: func(cmd *cobra.Command, args []string) {
+			run()
+		},
+	}
+)
+
+// fixme 在您看不见代码的时候运行迁移，我觉得是不安全的，所以编译后最好不要去执行迁移
+func init() {
+	StartCmd.PersistentFlags().StringVarP(&configYml, "config", "c", "config/settings.yml", "Start server with provided configuration file")
+	StartCmd.PersistentFlags().BoolVarP(&generate, "generate", "g", false, "generate migration file")
+	StartCmd.PersistentFlags().BoolVarP(&goAdmin, "goAdmin", "a", false, "generate easy-admin migration file")
+	StartCmd.PersistentFlags().StringVarP(&host, "domain", "d", "*", "select tenant host")
+}
+
+func run() {
+
+	if !generate {
+		fmt.Println(`start init`)
+		//1. 读取配置
+		config.Setup(
+			file.NewSource(file.WithPath(configYml)),
+			initDB,
+		)
+	} else {
+		fmt.Println(`generate migration file`)
+		_ = genFile()
+	}
+}
+
+func migrateModel() error {
+	if host == "" {
+		host = "*"
+	}
+	db := sdk.Runtime.GetDbByKey(host)
+	if config.DatabasesConfig[host].Driver == "mysql" {
+		//初始化数据库时候用
+		db.Set("gorm:table_options", "ENGINE=InnoDB CHARSET=utf8mb4")
+	}
+	err := db.Debug().AutoMigrate(&models.Migration{})
+	if err != nil {
+		return err
+	}
+	migration.Migrate.SetDb(db.Debug())
+	migration.Migrate.Migrate()
+	return err
+}
+func initDB() {
+	//3. 初始化数据库链接
+	database.Setup()
+	//4. 数据库迁移
+	fmt.Println("数据库迁移开始")
+	_ = migrateModel()
+	fmt.Println(`数据库基础数据初始化成功`)
+}
+
+func genFile() error {
+	t1, err := template.ParseFiles("template/migrate.template")
+	if err != nil {
+		return err
+	}
+	m := map[string]string{}
+	m["GenerateTime"] = strconv.FormatInt(time.Now().UnixNano()/1e6, 10)
+	m["Package"] = "version_local"
+	if goAdmin {
+		m["Package"] = "version"
+	}
+	var b1 bytes.Buffer
+	err = t1.Execute(&b1, m)
+	if goAdmin {
+		pkg.FileCreate(b1, "./cmd/migrate/migration/version/"+m["GenerateTime"]+"_migrate.go")
+	} else {
+		pkg.FileCreate(b1, "./cmd/migrate/migration/version-local/"+m["GenerateTime"]+"_migrate.go")
+	}
+	return nil
+}
