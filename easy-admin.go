62d0778ef5a8877e466f273fb7d5ffc9eceba4b8
Steve <nice.lizhi@gmail.com>
2023-11-02 08:29:51 +0800

    add easy-admin go file

---
+++ easy-admin.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/nicelizhi/easy-admin/cmd"
+)
+
+// @title Easy-Admin
+// @version 1.0.0
+// @description 基于Gin + Vue 的前后端分离权限管理系统的接口文档
+// @license.name Apache 2.0
+// @license.url https://github.com/nicelizhi/easy-admin/blob/master/LICENSE.md
+
+// @securityDefinitions.apikey Bearer
+// @in header
+// @name Authorization
+func main() {
+	cmd.Execute()
+}
