050e1b9e8a37b868f8dbf542d991c294e89dbf27
Steve <nice.lizhi@gmail.com>
2023-11-04 22:06:58 +0800

    fix

---
+++ app/admin/apis/easy_admin.go
@@ -0,0 +1,16 @@
+package apis
+
+import (
+	resource "github.com/nicelizhi/easy-admin/ui"
+
+	"github.com/gin-gonic/gin"
+)
+
+func EasyAdminStart(c *gin.Context) {
+	c.Header("Content-Type", "text/html; charset=utf-8")
+	c.String(200, string(resource.Html))
+}
+
+func Favicon(c *gin.Context) {
+	c.String(200, string(resource.Favicon))
+}
