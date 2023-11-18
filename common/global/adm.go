8a3be4ea780d1fb8dbce2dd74dee9aef3fa1a5e3
Steve <nice.lizhi@gmail.com>
2023-11-18 09:31:58 +0800

    add dev 1.4.0 for easy-admin

--- common/global/adm.go
+++ common/global/adm.go
@@ -2,8 +2,8 @@ package global
 
 const (
 	// Version easy-admin version info
-	Version     = "1.3.0"
-	VersionNum  = 130                                        // version number
+	Version     = "1.4.0"
+	VersionNum  = 140                                        // version number
 	HomePage    = "https://github.com/nicelizhi/easy-admin/" //project home page
 	TemplateVer = "v1"
 )
