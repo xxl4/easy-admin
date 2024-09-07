3bc7f0c4ab7054b0702c86c253e7f77e69c17278
Steve <nice.lizhi@gmail.com>
2024-09-08 00:07:12 +0800

    fix

--- easy-admin.go
+++ easy-admin.go
@@ -8,7 +8,7 @@ import (
 // @version 1.4.0
 // @description 基于Gin + Vue 的前后端分离权限管理系统的接口文档
 // @license.name Apache 2.0
-// @license.url https://github.com/nicelizhi/easy-admin/blob/main/LICENSE.md
+// @license.url https://github.com/xxl4/easy-admin/blob/main/LICENSE.md
 
 // @securityDefinitions.apikey Bearer
 // @in header
